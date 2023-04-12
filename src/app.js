65e343237349bb8f7c6fbc6c0487d536fa90c887
eliasloret <eliasloret12@gmail.com>
2023-04-12 23:38:21 +0000

    carta

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pinta = ["♣", "♠", "♥", "♦"];
 };
